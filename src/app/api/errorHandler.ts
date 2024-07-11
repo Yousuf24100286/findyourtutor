9926ca0a4331bbf6f9a5defe7d64c4e65fbb4369
Yousuf Khan <24100286@lums.edu.pk>
2024-07-11 13:36:43 +0500

    fix(user-auth-api): rename error.ts to errorHandler.ts
    - rename error.ts to errorHandler.ts due to compile time error in next.js

--- src/app/api/errorHandler.ts
+++ src/app/api/errorHandler.ts
@@ -7,6 +7,7 @@ export const NotImplementedError: ErrorResponse = RouteError(501, 'Not Implement
 export const MethodNotAllowedError: ErrorResponse = RouteError(405, 'Method Not Allowed.');
 export const InternalServerError: (error: unknown) => ErrorResponse = (error: unknown) => RouteError(500, 'An error occurred.', error);
 export const BadRequestError: (message: string) => ErrorResponse = (message: string) => RouteError(400, message);
+export const NotFoundError: (message: string) => ErrorResponse = (message: string) => RouteError(404, message);
 
 export const ErrorHandler = (error: unknown): ErrorResponse => {
 	if (error instanceof NextResponse) {
