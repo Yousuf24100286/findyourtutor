20698155cc8aa4a24b8d82dc656bff04401e5773
Yousuf Khan <24100286@lums.edu.pk>
2024-07-12 17:21:21 +0500

    feature(user-auth-api) - add role definition
    - add role definition to db schema
    - add role definition to api schema
    - valid roles are 'admin', 'tutor', 'student'

--- src/app/api/schema.ts
+++ src/app/api/schema.ts
@@ -6,7 +6,7 @@ export const AccountAPISchema = {
 			payload: z.object({
 				email: z.string().email(),
 				password: z.string().min(8),
-				role: z.string().min(1),
+				role: z.union([z.literal('tutor'), z.literal('student'), z.literal('admin')]),
 				group: z.string().min(1),
 			}),
 		}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,7 +6,7 @@ export const account = schema.table("account", {
   id: serial("id").primaryKey().unique().notNull(),
   email: varchar("email", { length: 256 }).notNull().unique(),
   password: varchar("password", { length: 256 }).notNull(),
-  role: varchar("role", { length: 256 }).notNull(),
+  role: varchar<"role", string, readonly ['tutor', 'student', 'admin']>("role", { length: 256 }).notNull(),
   group: varchar("group", { length: 256 }).notNull(),
   is_verified: boolean("is_verified").notNull().default(false),
   created_at: timestamp("created_at").notNull().defaultNow(),
