55d602f1d0d483506e6cf08f28c34461a368d8ff
Yousuf Khan <24100286@lums.edu.pk>
2024-07-16 14:18:11 +0500

    feat - added single password schema for whole application

--- src/app/api/schema.ts
+++ src/app/api/schema.ts
@@ -1,11 +1,20 @@
 import { z } from 'zod';
 
+export const PasswordInputSchema = z
+	.string({ message: 'Password must be a string' })
+	.min(8, { message: 'Password must be at least 8 characters long' })
+	.regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+	.regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+	.regex(/[0-9]/, { message: 'Password must contain at least one digit' }) 
+	.regex(/[~!@#$%^&*()-_+={}[\]|\\;:"<>,.\/\?]/, { message: 'Password must contain at least one special character' }); 
+;
+
 export const AccountAPISchema = {
 	signup: {
 		post: {
 			payload: z.object({
 				email: z.string().email(),
-				password: z.string().min(8),
+				password: PasswordInputSchema,
 			}).and(z.union([
 				z.object({
 					role: z.literal('tutor'),
