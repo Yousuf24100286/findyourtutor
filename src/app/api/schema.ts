9fbd4728ed85f5312b18e8800f93263947cde931
Yousuf Khan <24100286@lums.edu.pk>
2024-07-16 13:55:56 +0500

    feature(user-auth-api) add role and group definition in schema

--- src/app/api/schema.ts
+++ src/app/api/schema.ts
@@ -6,9 +6,20 @@ export const AccountAPISchema = {
 			payload: z.object({
 				email: z.string().email(),
 				password: z.string().min(8),
-				role: z.union([z.literal('tutor'), z.literal('student'), z.literal('admin')]),
-				group: z.string().min(1),
-			}),
+			}).and(z.union([
+				z.object({
+					role: z.literal('tutor'),
+					group: z.union([z.literal('enrolled'), z.literal('graduate')]).default('enrolled'),
+				}),
+				z.object({
+					role: z.literal('student'),
+					group: z.union([z.literal('student'), z.literal('parent')]).default('student'),
+				}),
+				z.object({
+					role: z.literal('admin'),
+					group: z.literal('admin').default('admin'),
+				})
+			]))
 		}
 	},
 	signin: {
