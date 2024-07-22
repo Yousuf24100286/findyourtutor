88870c4144203a535d6a75d1c2fa62c86d9003bd
Yousuf Khan <24100286@lums.edu.pk>
2024-07-22 16:01:57 +0500

    fix: remove default shadcn css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,40 +27,12 @@ const config = {
         },
         'link': {
           DEFAULT: '#0C5DFF',
-        },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },      
+        }, 
+      },
+      textColor: {
+        'primary': '#040921',
+        'secondary': "#04092160",
+        'disabled': "#04092132",
       },
       container: {
         center: true,
