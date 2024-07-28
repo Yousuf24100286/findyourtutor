b2d7cc9dcaf1e085eff5d89fad1d918e853edffb
Yousuf Khan <24100286@lums.edu.pk>
2024-07-28 12:10:54 +0500

    fix: fix forgot password card text

--- src/app/auth/forgot-password/reset/page.tsx
+++ src/app/auth/forgot-password/reset/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 
-const ForgotPasswordForm = async () => (
+const ResetPasswordForm = async () => (
 	<form method='POST' className='w-full flex flex-col gap-6' action={async (formData: FormData) => {
 		'use server'
 		const email = formData.get('email') as string;
@@ -29,7 +29,7 @@ export default async ({ searchParams }: Readonly<{ searchParams: unknown }>) => 
 	// To Do: 
 	// extract token from searchParams
 	// verify token
-	// if token is valid, render ForgotPasswordForm
+	// if token is valid, render ResetPasswordForm
 	// else redirect to /auth/forgot-password
 
 	return (
@@ -37,8 +37,8 @@ export default async ({ searchParams }: Readonly<{ searchParams: unknown }>) => 
 			<div className='flex flex-row justify-center'>
 				<div className="flex flex-col items-center bg-white min-w-96 rounded-lg gap-4 pt-6 pb-9 px-10">
 					<H2 className='flex items-center'>Reset Password</H2>
-					<P>Enter your email address and we will send you a link to reset your password.</P>
-					<ForgotPasswordForm />
+					<P>Enter your new password and confirm it to reset your password.</P>
+					<ResetPasswordForm />
 				</div>
 			</div>	
 		</div>
